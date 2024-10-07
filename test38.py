a78f683fd0fe60e25676fd6572fe411e9cbb8c04
soryhon <soryhon2@gmail.com>
2024-10-08 00:21:18 +0900

    Update test38.py

--- test38.py
+++ test38.py
@@ -600,10 +600,10 @@ with st.expander("요청사항 리스트", expanded=True):
                         
                         # 엑셀 파일인 경우 시트 선택 로직을 추가
                         if file_type == 'xlsx':
-                            
+                            html_report_set = f"<div style='text-indent: 5px;'>\n"
                             file_data_dict = handle_file_selection(file_path, file_content, file_type, idx)
                             if file_data_dict is not None:
-                                html_report_set = f"<div style='text-indent: 5px;'>\n"
+                                
                                 # 제목 입력 값 가져오기
                                 html_report_set +=  f"<h3>{idx + 1}. {row['제목']}</h3>\n"
                                 row['파일'] = f"/{st.session_state['github_repo']}/{st.session_state['github_branch']}/{selected_file}"
@@ -612,7 +612,7 @@ with st.expander("요청사항 리스트", expanded=True):
                                     ws = wb[sheet_name]
                                     html_data = convert_df_to_html_with_styles_and_merging(ws, df)
                                     html_report_set += f"<div style='text-indent: 20px;'>{html_data}</div>\n"
-                                html_report_set += "</div>\n"       
+                                #html_report_set += "</div>\n"       
                             #row['파일데이터'] = html_report_set
                             # map 변수에 idx가 키, html_report_set 값으로 저장
                             # global_report_map[idx] = html_report_set
@@ -625,7 +625,7 @@ with st.expander("요청사항 리스트", expanded=True):
                                 # 제목 입력 값 가져오기
                                 html_report_set +=  f"<h3>{idx + 1}. {row['제목']}</h3>\n"
                                 html_report_set += f"<p>{file_data}</p>"                        
-                                html_report_set += "</div>\n"       
+                        html_report_set += "</div>\n"       
                         row['파일데이터'] = html_report_set
                         report_html = ""
                         if "html_report" in st.session_state:
