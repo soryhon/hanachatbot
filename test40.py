ef15a94428ac1f4ce5c4d05808b70be1146d2a54
soryhon <soryhon2@gmail.com>
2024-10-10 22:22:50 +0900

    Update test40.py

--- test40.py
+++ test40.py
@@ -669,7 +669,9 @@ if github_info_loaded:
             st.session_state['upload_folder'] = f"uploadFiles/{selected_folder}"
             st.session_state['selected_folder_name'] = f"{selected_folder}"
             refresh_page()
-            
+            st.success(f"[{selected_folder}] 보고서가 선택되었습니다.")
+        else:   
+            st.warring("보고서 주래를 선택하세요.")
             
     with col2:        
         new_folder_name = st.text_input("새 폴더명 입력", max_chars=20, key="new_folder_name", value=st.session_state['new_folder_text'])
