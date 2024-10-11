0bf700fc607537ee4cfc5d862008313f99cafd94
soryhon <soryhon2@gmail.com>
2024-10-12 02:25:24 +0900

    Update test39.py

--- test39.py
+++ test39.py
@@ -963,11 +963,9 @@ with col1:
     st.write("")
 with col2:   
     report_title = "작성할 보고서를 선택하세요."
-    title_style = "font-size:14px; font-weight:normal; color:#cccccc;border: 1px solid #dddddd;"
     if 'selected_folder_name' in st.session_state:
         if st.session_state['selected_folder_name'] != folderlist_init_value:
             report_title = " [" + st.session_state['selected_folder_name'] + "] 보고서"
-            title_style = "font-size:20px; font-weight:bold; color:#000000;border: 0px solid #dddddd;"
     st.markdown(
         f"<div style='text-align:center;font-size:20px; font-weight:bold; color:#000000;border: 0px solid #dddddd;border-radius: 10px;width:100%;padding 10px;'>{report_title}</div>",
         unsafe_allow_html=True
