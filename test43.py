09b5e9c3bbc8dfd7d3bca11ec8b5742efad76484
soryhon <soryhon2@gmail.com>
2024-10-12 13:47:43 +0900

    Update test43.py

--- test43.py
+++ test43.py
@@ -846,12 +846,12 @@ refresh_page()
 col1, col2 = st.columns([0.7,0.3])
 with col1:
     st.markdown(
-        "<p style='font-size:25px; font-weight:bold; color:#000000;'>척척하나 - 일일 업무 보고서 자동 완성</p>",
+        "<p style='font-size:25px; font-weight:bold; color:#000000;'> 일일 업무 보고서 자동 완성</p>",
         unsafe_allow_html=True
     )
 with col2:
     st.markdown(
-        "<p style='font-size:13px; font-weight:bold; color:#aaaaaa; margin-top:10px;'>by CheokCeock1 prototype v.1</p>",
+        "<p style='font-size:13px; font-weight:bold; color:#aaaaaa; margin-top:10px;'>by CheokCeock<b style='font-size:18px;color:#009999'>1</b> prototype v.1</p>",
         unsafe_allow_html=True
     )
 
