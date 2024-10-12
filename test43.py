ff3df4c420be39a2e809eacc4a79848d6e488e9e
soryhon <soryhon2@gmail.com>
2024-10-12 17:47:31 +0900

    Update test43.py

--- test43.py
+++ test43.py
@@ -859,54 +859,66 @@ with col2:
 # 보고서 주제 및 폴더 선택, 새 폴더 만들기
 if github_info_loaded:
     with st.expander("보고서 선택", expanded=st.session_state['check_report']):
-        #tab1, tab2 = st.tabs(["보고서 주제", "양식 불러오기"])
-        #with tab1:
-        col1, col2, col3, col4 = st.columns([0.2, 0.3, 0.05, 0.35])
-        with col1:
-            st.write("")
-            st.markdown(
-                "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>보고서 주제<br/>선택 / 등록</p>",
-                unsafe_allow_html=True
-            )
-        with col2:
-            folder_list = get_folder_list_from_github(st.session_state['github_repo'], st.session_state['github_branch'], st.session_state['github_token'])
-            # st.selectbox 위젯 생성 (이제 session_state['selected_folder'] 사용 가능)
-
-            # 'selected_folder'가 folder_list에 있을 때만 index 설정
-            selected_index = st.session_state['selected_folder_index']
-            if st.session_state['selected_folder_name'] in folder_list:
-                selected_index = folder_list.index(st.session_state['selected_folder_name']) + 1
-            #else:
-                #selected_index = 0  # 기본값으로 '주제를 선택하세요.' 선택
-            st.session_state['selected_folder_index'] = selected_index
-            st.session_state['folder_list_option'] = [folderlist_init_value] + folder_list
-            # 폴더 선택 selectbox 생성 (새 폴더 추가 후, 선택값으로 설정)
-            selected_folder = st.selectbox(
-                "보고서 주제 리스트",
-                options=st.session_state['folder_list_option'],  # 옵션 리스트에 새 폴더 반영
-                index=st.session_state['selected_folder_index'],  # 새로 선택된 폴더를 기본값으로 선택
-                key="selected_folder"
-            )
-            # 파일 업로드와 요청사항 리스트의 기본 폴더 설정
-            if selected_folder != "주제를 선택하세요.":
-                st.session_state['upload_folder'] = f"uploadFiles/{selected_folder}"
-                st.session_state['selected_folder_name'] = f"{selected_folder}"
-                refresh_page()
-                st.session_state['check_report']=False
-                st.session_state['check_count']=True
-                #st.success(f"[{selected_folder}] 보고서가 선택되었습니다.")
-            #else:   
-                #st.warning("보고서 주제를 선택하세요.")
-        with col3:
-            st.markdown(
-                "<p style='font-size:20px; font-weight:bold; color:#dddddd;text-align:center;margin-top:28px;'>|</p>",
-                unsafe_allow_html=True
-            )
-        with col4:
-            col1, col2 = st.columns([0.7, 0.3])
+        tab1, tab2, tab3 = st.tabs(["보고서 주제 리스트 선택", "저장된 보고서 양식 불러오기","새로운 보고서명 만들기"])
+        with tab1:
+            col1, col2 = st.columns([0.2, 0.8])
             with col1:
-                new_folder_name = st.text_input("새로 등록할 보고서명 입력", max_chars=20, key="new_folder_name", value=st.session_state['new_folder_text'])
+                st.write("")
+                st.markdown(
+                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>보고서 주제<br/>선택 / 등록</p>",
+                    unsafe_allow_html=True
+                )
             with col2:
+                folder_list = get_folder_list_from_github(st.session_state['github_repo'], st.session_state['github_branch'], st.session_state['github_token'])
+                # st.selectbox 위젯 생성 (이제 session_state['selected_folder'] 사용 가능)
+    
+                # 'selected_folder'가 folder_list에 있을 때만 index 설정
+                selected_index = st.session_state['selected_folder_index']
+                if st.session_state['selected_folder_name'] in folder_list:
+                    selected_index = folder_list.index(st.session_state['selected_folder_name']) + 1
+                #else:
+                    #selected_index = 0  # 기본값으로 '주제를 선택하세요.' 선택
+                st.session_state['selected_folder_index'] = selected_index
+                st.session_state['folder_list_option'] = [folderlist_init_value] + folder_list
+                # 폴더 선택 selectbox 생성 (새 폴더 추가 후, 선택값으로 설정)
+                selected_folder = st.selectbox(
+                    "보고서 주제 리스트",
+                    options=st.session_state['folder_list_option'],  # 옵션 리스트에 새 폴더 반영
+                    index=st.session_state['selected_folder_index'],  # 새로 선택된 폴더를 기본값으로 선택
+                    key="selected_folder"
+                )
+                # 파일 업로드와 요청사항 리스트의 기본 폴더 설정
+                if selected_folder != "주제를 선택하세요.":
+                    st.session_state['upload_folder'] = f"uploadFiles/{selected_folder}"
+                    st.session_state['selected_folder_name'] = f"{selected_folder}"
+                    refresh_page()
+                    st.session_state['check_report']=False
+                    st.session_state['check_count']=True
+                    #st.success(f"[{selected_folder}] 보고서가 선택되었습니다.")
+                #else:   
+                    #st.warning("보고서 주제를 선택하세요.")
+        with tab2:
+            col1, col2 = st.columns([0.21, 0.79])
+            with col1:
+                st.write("")
+                st.markdown(
+                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>저장된 보고서<br/>양식 불러오기</p>",
+                    unsafe_allow_html=True
+                )
+            with col2:    
+                load_template_button_function()
+
+        with tab3:
+            col1, col2, col3 = st.columns([0.2, 0.4,0.4])
+            with col1:
+                st.write("")
+                st.markdown(
+                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>보고서 주제<br/>새로 만들기기</p>",
+                    unsafe_allow_html=True
+                )
+            with col2:
+                new_folder_name = st.text_input("새로 등록할 보고서명 입력", max_chars=20, key="new_folder_name", value=st.session_state['new_folder_text'])
+            with col3:
                 st.markdown(
                     "<p style='font-size:18px; margin-top:27px;'></p>",
                     unsafe_allow_html=True
@@ -931,21 +943,11 @@ if github_info_loaded:
                             refresh_page()
                             init_session_state(True)
                             st.success("성공")            
-        st.markdown(
-            "<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
-            unsafe_allow_html=True
-        )
-        #with tab2:
-        col1, col2 = st.columns([0.21, 0.79])
-        with col1:
-            st.write("")
-            st.markdown(
-                "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>저장된 보고서<br/>양식 불러오기</p>",
-                unsafe_allow_html=True
-            )
-        with col2:    
-            load_template_button_function()
-
+        #st.markdown(
+            #"<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
+            #unsafe_allow_html=True
+        #)
+      
 else:
     st.warning("GitHub 정보가 설정되지 않았습니다. 먼저 GitHub Token을 입력해 주세요.")
 
