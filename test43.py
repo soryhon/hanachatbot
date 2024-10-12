914a54d520757a929160941d1a538efc48afb576
soryhon <soryhon2@gmail.com>
2024-10-12 17:57:18 +0900

    Update test43.py

--- test43.py
+++ test43.py
@@ -859,19 +859,13 @@ with col2:
 # 보고서 주제 및 폴더 선택, 새 폴더 만들기
 if github_info_loaded:
     with st.expander("보고서 선택", expanded=st.session_state['check_report']):
-        tab1, tab2, tab3 = st.tabs([
-            st.markdown(
-                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;border:1px solid #dddddd;border-bottom:0px;margin-righnt:5px;'>보고서 주제 선택 </p>",
-                    unsafe_allow_html=True
-                ), 
-                                    
-                                    "저장된 보고서 양식 불러오기","새로운 보고서명 만들기"])
+        tab1, tab2, tab3 = st.tabs(["등록된 보고서명 선택하기", "저장된 보고서 양식 불러오기","새로운 보고서명 만들기"])
         with tab1:
             col1, col2 = st.columns([0.2, 0.8])
             with col1:
                 st.write("")
                 st.markdown(
-                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;border:1px solid #dddddd;border-bottom:0px;margin-righnt:5px;'>보고서 주제 선택 </p>",
+                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>등록된 보고서명<br/>선택 </p>",
                     unsafe_allow_html=True
                 )
             with col2:
@@ -908,7 +902,7 @@ if github_info_loaded:
             with col1:
                 st.write("")
                 st.markdown(
-                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;border:1px solid #dddddd;border-bottom:0px;margin-righnt:5px;'>저장된 보고서<br/>양식 불러오기</p>",
+                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>저장된 보고서<br/>양식 불러오기</p>",
                     unsafe_allow_html=True
                 )
             with col2:    
@@ -919,7 +913,7 @@ if github_info_loaded:
             with col1:
                 st.write("")
                 st.markdown(
-                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;border:1px solid #dddddd;border-bottom:0px;margin-righnt:5px;'>보고서 주제<br/>새로 만들기기</p>",
+                    "<p style='font-size:14px; font-weight:bold; color:#000000;text-align:center;'>t새로운 보고서명<br/>만들기</p>",
                     unsafe_allow_html=True
                 )
             with col2:
