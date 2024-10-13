9ff8b68a704328124fd596a63d93e9e24ee5a98e
soryhon <soryhon2@gmail.com>
2024-10-13 23:06:10 +0900

    Update test51.py

--- test51.py
+++ test51.py
@@ -250,8 +250,8 @@ with st.expander("📊 결과 보고서 보기", expanded=st.session_state['chec
 
         for idx, response in enumerate(st.session_state["response"]):
             #st.text_area(f"응답 {idx+1}:", value=response, height=300)
-            chartcode = bd.extract_text_within_brackets(response)
-            html_response_value = f"<div style='border: 0px solid #cccccc; padding: 1px;'>{response}</div>"
+            response_text, chartcode = bd.extract_text_within_brackets(response)
+            html_response_value = f"<div style='border: 0px solid #cccccc; padding: 1px;'>{response_text}</div>"
             html_result_value += html_response_value
             st.components.v1.html(html_response_value, height=1024, scrolling=True)
       
