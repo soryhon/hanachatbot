b930fc9a2b550ae096a8e6684e34fadd9aacdbde
soryhon <soryhon2@gmail.com>
2024-10-13 22:55:39 +0900

    Update test51.py

--- test51.py
+++ test51.py
@@ -262,7 +262,7 @@ with st.expander("📊 결과 보고서 보기", expanded=st.session_state['chec
             )   
             ##st.code(chartcode, language='python')  # 추출한 Python 코드를 화면에 출력
             st.markdown(
-                  "<p style='font-size:20px;font-weight:bold;color:#008080;padding:5px;'>📈 AI 차트 추천</p>",
+                  "<p style='font-size:22px;font-weight:bold;color:#008080;padding:5px;'>📈 AI 차트 추천</p>",
                   unsafe_allow_html=True
               )
             # eval()을 사용하여 Python 코드를 실행하고 차트를 출력
