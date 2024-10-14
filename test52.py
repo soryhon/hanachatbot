79b4b4fd2a28ceeeda021558f755f36c8913b80a
soryhon <soryhon2@gmail.com>
2024-10-15 00:25:38 +0900

    Update test52.py

--- test52.py
+++ test52.py
@@ -298,19 +298,19 @@ with st.expander("✍️ 요청사항 리스트", expanded=st.session_state['che
 
     for idx, row in enumerate(rows):
         with st.container():
-            #col1, col2 = st.columns([0.01, 0.99]) 
-            #with col1:
-                #row_checked = st.checkbox("", key=f"row_checked_{idx}", value=row.get("checked", False))  # 체크박스만 추가
-                #st.write("")
-            #with col2:
-            st.markdown(
-                f"<p style='font-size:16px; font-weight:bold; color:#000000; margin-top:5px;'>{idx+1}. 요청사항</p>",
-                unsafe_allow_html=True
-            )
+          #col1, col2 = st.columns([0.01, 0.99]) 
+          #with col1:
+              #row_checked = st.checkbox("", key=f"row_checked_{idx}", value=row.get("checked", False))  # 체크박스만 추가
+              #st.write("")
+          #with col2:
+          st.markdown(
+              f"<p style='font-size:16px; font-weight:bold; color:#000000; margin-top:5px;'>{idx+1}. 요청사항</p>",
+              unsafe_allow_html=True
+          )
         
-            row['제목'] = st.text_input(f"제목 : '{idx+1}.요청사항'의 제목을 입력해주세요.", row['제목'], key=f"title_{idx}")
-            row['요청'] = st.text_area(f"요청 : '{idx+1}.요청사항'의 요청할 내용을 입력해주세요.", row['요청'], key=f"request_{idx}")
-            row['주소'] = st.text_input(f"주소 : '{idx+1}.요청사항'의 동영상 URL를 입력해주세요.", row['주소'], key=f"url_{idx}")
+          row['제목'] = st.text_input(f"제목 : '{idx+1}.요청사항'의 제목을 입력해주세요.", row['제목'], key=f"title_{idx}")
+          row['요청'] = st.text_area(f"요청 : '{idx+1}.요청사항'의 요청할 내용을 입력해주세요.", row['요청'], key=f"request_{idx}")
+          row['주소'] = st.text_input(f"주소 : '{idx+1}.요청사항'의 동영상 URL를 입력해주세요.", row['주소'], key=f"url_{idx}")
             
         
 # 8 프레임
