ee8283cbb3f63daf74d3df001bbb61167fa61989
soryhon <soryhon2@gmail.com>
2024-10-15 01:17:10 +0900

    Update test52.py

--- test52.py
+++ test52.py
@@ -372,6 +372,10 @@ st.markdown(
 # 결과 보고서 LLM 응답 보기/ 결과 보고서 저장/ 보고서 양식 저장
 html_result_value = "<div id='html_result_value'>"
 with st.expander("📊 결과 보고서 보기", expanded=st.session_state['check_result']):
+     if "html_report" in st.session_state:
+          st.write("파일 데이터 추출 보기")
+          html_report_value = f"<div style='border: 2px solid #cccccc; padding: 2px;'>{st.session_state['html_report']}</div>"
+          st.components.v1.html(html_report_value, height=10240, scrolling=True)
      if "response" in st.session_state:
         st.markdown(
             "<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
