f7683dd60990ea14121770d73603acd73c4bba58
soryhon <soryhon2@gmail.com>
2024-10-16 23:11:20 +0900

    Update backend1.py

--- backend1.py
+++ backend1.py
@@ -1196,7 +1196,7 @@ def extract_transcript_from_youtube(video_url):
         
         # YouTube 영상 자막 추출
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko', 'en'])
-        return transcript
+        return extract_text_from_transcript(transcript)
     except TranscriptsDisabled as e:
         st.error(f"해당 동영상에는 자막이 비활성화되어 있습니다: {str(e)}")
         return None
@@ -1204,6 +1204,19 @@ def extract_transcript_from_youtube(video_url):
         st.error(f"동영상 자막을 추출하는 데 실패했습니다: {str(e)}")
         return None
 
+def extract_text_from_transcript(transcript):
+    try:
+        # 텍스트만 추출하여 리스트로 저장
+        text_data = [item['text'] for item in transcript]
+        
+        # 텍스트를 하나의 문자열로 연결
+        combined_text = ' '.join(text_data)
+        
+        return combined_text
+    except Exception as e:
+        st.error(f"자막에서 텍스트를 추출하는 데 실패했습니다: {str(e)}")
+        return None
+        
 # 주어진 동영상 URL에서 자막을 추출하여 반환하는 함수.
 def extract_text_from_video_url(url, language="ko", add_video_info=True):
     try:
