607d70d777be27dbff5c8ef8a42e16b889f1a345
soryhon <soryhon2@gmail.com>
2024-10-18 00:24:56 +0900

    Update backend1.py

--- backend1.py
+++ backend1.py
@@ -1320,7 +1320,7 @@ def convert_m4a_to_wav(file_content):
         wav_path = temp_wav_file.name
 
         # ffmpeg을 사용하여 m4a -> wav 변환
-        command = ['hanachatbot', '-i', m4a_path, wav_path]
+        command = ['soryhon/hanachatbot/ffmpeg/ffmpeg', '-i', m4a_path, wav_path]
         subprocess.run(command, check=True)
 
         # 변환된 wav 파일 열기
@@ -1355,8 +1355,8 @@ def extract_text_from_audio(file_content, file_type):
     # m4a 파일은 wav로 변환
     if file_type == 'm4a':
         st.write("m4a 파일을 변환 중입니다...")
-        #file_content = convert_m4a_to_wav(file_content)
-        file_content = convert_m4a_to_wav_from_install(file_content)
+        file_content = convert_m4a_to_wav(file_content)
+        #file_content = convert_m4a_to_wav_from_install(file_content)
         if file_content is None:
             st.write("m4a 파일을 None")
             return None
