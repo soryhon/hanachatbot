e8e0db40464b3d7864972f22fcc3aac6acf04a76
soryhon <soryhon2@gmail.com>
2024-10-19 22:50:26 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,7 +3,7 @@ import importlib
 import os
 
 # 사이드바에 메뉴 추가
-page = st.sidebar.selectbox('Choose a page', ['Home', '업무 보고서 자동 완성', '보고서 비교분석 자동 완성', '음성 파일 보고서 완성'])
+page = st.sidebar.radio('Choose a page', ['Home', '업무 보고서 자동 완성', '보고서 비교분석 자동 완성', '음성 파일 보고서 완성'])
 
 # 홈 페이지 설정
 if page == 'Home':
