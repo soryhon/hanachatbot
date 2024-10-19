e97b21a9af2c7702b117a88992aed85c49d01929
soryhon <soryhon2@gmail.com>
2024-10-19 12:57:09 +0900

    Update test53.py

--- test53.py
+++ test53.py
@@ -464,77 +464,82 @@ st.markdown(
 # 결과 보고서 LLM 응답 보기/ 결과 보고서 저장/ 보고서 양식 저장
 html_result_value = "<div id='html_result_value'>"
 with st.expander("📊 결과 보고서 보기", expanded=st.session_state['check_result']):
-     if "response" in st.session_state:
-        st.markdown(
-            "<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
-            unsafe_allow_html=True
-        )  
-        st.session_state['check_result'] = True
-        for idx, response in enumerate(st.session_state["response"]):
-            #st.text_area(f"응답 {idx+1}:", value=response, height=300)
-
-            html_response_value = f"<div style='border: 0px solid #cccccc; padding: 1px;'>{response}</div>"
-            html_result_value += html_response_value
-            st.components.v1.html(html_response_value, height=1024, scrolling=True)
-     html_result_value += "</div>"
-     st.markdown(
-          "<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
-        unsafe_allow_html=True
-     )
-    
-# 결과 저장 버튼
-     col1, col2 = st.columns([0.5, 0.5])
-     with col1:   
-        if st.button("💾 결과 내용 저장", key="save_result", use_container_width=True):
+    tab1, tab2, tab3 = st.tabs(["• AI 요약 보고서 ", "• 음성파일 텍스트 보기"])
+    with tab1:   
+        if "response" in st.session_state:
+            st.markdown(
+                "<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
+                unsafe_allow_html=True
+            )  
             st.session_state['check_result'] = True
-            st.session_state['check_report'] = False
-            st.session_state['check_upload'] = False
-            st.session_state['check_setting'] = False
-            st.session_state['check_request'] = False
-            if "response" in st.session_state:                
-                
-                folder_name = st.session_state['selected_folder_name']
-                report_date_str = st.session_state.get('report_date_str', datetime.datetime.now().strftime('%Y%m%d'))
-                
-                # save_html_response 함수를 사용하여 HTML 파일 저장
-                file_name, temp_file_path = bd.save_html_response(html_result_value, folder_name, report_date_str)
-
-                # 파일 저장 경로 (reportFiles/{폴더명}/{일자})
-                github_folder = f"reportFiles/{folder_name}/{report_date_str}"
-
-                # 폴더 존재 확인 및 생성
-                bd.check_and_create_github_folder(github_folder, st.session_state['github_repo'], st.session_state['github_branch'], st.session_state['github_token'])
-                
-                # GitHub에 HTML 파일 저장
-                sha = bd.get_file_sha(st.session_state['github_repo'], f"{github_folder}/{file_name}", st.session_state['github_token'], branch=st.session_state['github_branch'])
-                bd.upload_file_to_github(st.session_state['github_repo'], github_folder, file_name, open(temp_file_path, 'rb').read(), st.session_state['github_token'], branch=st.session_state['github_branch'], sha=sha)
+            for idx, response in enumerate(st.session_state["response"]):
+                #st.text_area(f"응답 {idx+1}:", value=response, height=300)
+    
+                html_response_value = f"<div style='border: 0px solid #cccccc; padding: 1px;'>{response}</div>"
+                html_result_value += html_response_value
+                st.components.v1.html(html_response_value, height=1024, scrolling=True)
+         html_result_value += "</div>"
+         st.markdown(
+              "<hr style='border-top:1px solid #dddddd;border-bottom:0px solid #dddddd;width:100%;padding:0px;margin:0px'></hr>",
+            unsafe_allow_html=True
+         )
+        
+    # 결과 저장 버튼
+         col1, col2 = st.columns([0.5, 0.5])
+         with col1:   
+            if st.button("💾 결과 내용 저장", key="save_result", use_container_width=True):
                 st.session_state['check_result'] = True
-                st.success(f"{file_name} 파일이 생성되었습니다.")
-                if st.download_button(
-                    label="📥 다운로드",
-                    use_container_width=True,
-                    data=open(temp_file_path, 'r', encoding='utf-8').read(),
-                    file_name=file_name,
-                    mime="text/html"
-                ):
+                st.session_state['check_report'] = False
+                st.session_state['check_upload'] = False
+                st.session_state['check_setting'] = False
+                st.session_state['check_request'] = False
+                if "response" in st.session_state:                
+                    
+                    folder_name = st.session_state['selected_folder_name']
+                    report_date_str = st.session_state.get('report_date_str', datetime.datetime.now().strftime('%Y%m%d'))
+                    
+                    # save_html_response 함수를 사용하여 HTML 파일 저장
+                    file_name, temp_file_path = bd.save_html_response(html_result_value, folder_name, report_date_str)
+    
+                    # 파일 저장 경로 (reportFiles/{폴더명}/{일자})
+                    github_folder = f"reportFiles/{folder_name}/{report_date_str}"
+    
+                    # 폴더 존재 확인 및 생성
+                    bd.check_and_create_github_folder(github_folder, st.session_state['github_repo'], st.session_state['github_branch'], st.session_state['github_token'])
+                    
+                    # GitHub에 HTML 파일 저장
+                    sha = bd.get_file_sha(st.session_state['github_repo'], f"{github_folder}/{file_name}", st.session_state['github_token'], branch=st.session_state['github_branch'])
+                    bd.upload_file_to_github(st.session_state['github_repo'], github_folder, file_name, open(temp_file_path, 'rb').read(), st.session_state['github_token'], branch=st.session_state['github_branch'], sha=sha)
                     st.session_state['check_result'] = True
-                    st.session_state['check_report'] = False
-                    st.session_state['check_upload'] = False
-                    st.session_state['check_setting'] = False
-                    st.session_state['check_request'] = False
-
-            else:
-                st.warning("결과 보고서를 먼저 실행하세요.")
-     with col2:
-          if st.button("🗃️ 보고서 양식 저장", key="save_template", use_container_width=True):
-               st.session_state['check_result'] = True
-               st.session_state['check_report'] = False
-               st.session_state['check_upload'] = False
-               st.session_state['check_setting'] = False
-               st.session_state['check_request'] = False
-               bd.save_template_to_json()
-
-
+                    st.success(f"{file_name} 파일이 생성되었습니다.")
+                    if st.download_button(
+                        label="📥 다운로드",
+                        use_container_width=True,
+                        data=open(temp_file_path, 'r', encoding='utf-8').read(),
+                        file_name=file_name,
+                        mime="text/html"
+                    ):
+                        st.session_state['check_result'] = True
+                        st.session_state['check_report'] = False
+                        st.session_state['check_upload'] = False
+                        st.session_state['check_setting'] = False
+                        st.session_state['check_request'] = False
+    
+                else:
+                    st.warning("결과 보고서를 먼저 실행하세요.")
+         with col2:
+              if st.button("🗃️ 보고서 양식 저장", key="save_template", use_container_width=True):
+                   st.session_state['check_result'] = True
+                   st.session_state['check_report'] = False
+                   st.session_state['check_upload'] = False
+                   st.session_state['check_setting'] = False
+                   st.session_state['check_request'] = False
+                   bd.save_template_to_json()
+    with tab2:  
+        if "html_report" in st.session_state:
+            #st.write("파일 데이터 추출 보기")
+            html_report_value = f"<div style='border: 0px solid #cccccc; padding: 2px;'>{st.session_state['html_report']}</div>"
+            st.components.v1.html(html_report_value, height=10240, scrolling=True)
 # 11 프레임
 # 결과 보고서 HTML 보기
 #if "html_report" in st.session_state:
