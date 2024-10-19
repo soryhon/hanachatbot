739934fbfe8e295ee80df88705483a079abeaba0
soryhon <soryhon2@gmail.com>
2024-10-20 02:06:02 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -52,7 +52,11 @@ def exec_page(file_name):
 selected_menu(0)
 
 if st.sidebar.button("보고서 자동 완성", use_container_width=True):
-    selected_menu(0)
+    if st.session_state['menu01']:
+        st.session_state['menu01']=False
+    else:
+        selected_menu(0)
+        
 if st.session_state['menu01']:
     with st.sidebar.form("보고서 자동 완성", clear_on_submit=True):
         if st.form_submit_button("업무 보고서 자동 완성", use_container_width=True):        
