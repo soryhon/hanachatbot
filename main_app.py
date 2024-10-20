168756dcb962134a5cf473bf94764fbe7b2def99
soryhon <soryhon2@gmail.com>
2024-10-20 13:39:02 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -51,6 +51,7 @@ def exec_page(file_name):
         
 selected_menu(0)
 
+file_list=["test50.py","test51.py","test53.py",""]
 # 사이드바에 메뉴 추가
 st.sidebar.markdown(
     """
@@ -70,14 +71,16 @@ st.sidebar.markdown(
     unsafe_allow_html=True
 )
 
+with st.sidebar.form("menu01_form", clear_on_submit=False):
+    selected_menu2 = st.select_slider("selectbox", ['보고서 자동 완성', '결과 보고서 현황', '챌린지5팀 소개', '만족도 평가'])
+    if st.form_submit_button("업무 보고서 자동 완성", use_container_width=True):        
+        selected_menu(0)
+        
 with st.sidebar.form("보고서 자동 완성1", clear_on_submit=False):
     selected_menu = st.radio("radio", ['보고서 자동 완성', '결과 보고서 현황', '챌린지5팀 소개', '만족도 평가'])
     if st.form_submit_button("업무 보고서 자동 완성", use_container_width=True):        
         selected_menu(0)
-with st.sidebar.form("보고서 자동 완성2", clear_on_submit=False):
-    selected_menu2 = st.selectbox("selectbox", ['보고서 자동 완성', '결과 보고서 현황', '챌린지5팀 소개', '만족도 평가'])
-    if st.form_submit_button("업무 보고서 자동 완성", use_container_width=True):        
-        selected_menu(0)
+
 with st.sidebar.form("보고서 자동 완성", clear_on_submit=False):
 
     if st.form_submit_button("업무 보고서 자동 완성", use_container_width=True):        
