95605a87e237b2b66597bc047fbd6e755dd3a400
soryhon <soryhon2@gmail.com>
2024-10-21 19:51:45 +0900

    Update test57.py

--- test57.py
+++ test57.py
@@ -70,13 +70,13 @@ col1,col2,col3,col4,col5 = st.columns([0.2,0.2,0.2,0.2,0.2])
 with col1:
     st.image("image/star01.png", width=100)
 with col2:
-    st.write("☆☆☆☆☆")
+    st.image("image/star01.png", width=100)
 with col3:
-    st.write("☆☆☆☆☆")
+    st.image("image/star01.png", width=100)
 with col4:
-    st.write("☆☆☆☆☆")
+    st.image("image/star01.png", width=100)
 with col5:
-    st.write("☆☆☆☆☆")
+    st.image("image/star01.png", width=100)
 # 별점 선택 (슬라이더 사용)
 score = st.slider("별점 선택 (1~5, 0.5 단위):", 1.0, 5.0, 0.5)
 
