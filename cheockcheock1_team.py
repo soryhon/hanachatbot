7abe330ccefce454fc77be5a4467d2fbda19ca73
soryhon <soryhon2@gmail.com>
2024-10-22 01:49:17 +0900

    Update cheockcheock1_team.py

--- cheockcheock1_team.py
+++ cheockcheock1_team.py
@@ -20,8 +20,8 @@ with col2:
 st.markdown("<hr style='border-top:1px solid #dddddd;'>", unsafe_allow_html=True)
 st.markdown("## 프로젝트 소개")    
 with st.container():
-    st.image("checkcheck.jpg", width=150)
-    st.markdown("### CheokCeock1")
+    st.image("cheockcheock.jpg", width=300)
+    st.markdown("### CheockCheock1")
     st.markdown("""
         **추진 배경**  
         **매일 반복적이고 고정된 형식의 업무 처리 및 보고서 작성 수행을 보다 효율적으로 자동화하여 시간 절약 및 정확도를 개선 하기 위함.**  
