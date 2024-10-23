eb86d0e884cf5dea22a6927c159e63e1a4626066
soryhon <soryhon2@gmail.com>
2024-10-23 23:33:43 +0900

    Update cheockcheock1_03.py

--- cheockcheock1_03.py
+++ cheockcheock1_03.py
@@ -239,6 +239,18 @@ st.markdown(
 with st.expander("⚙️ 요청사항 설정 / 파일 업로드", expanded=st.session_state['check_setting_03']):
     tab1, tab2 = st.tabs(["• 요청사항 및 기준일자 설정", "• ⬆️ 음성 파일 업로드"]) 
     with tab1:
+        col1, col2 = st.columns([0.5, 0.5])
+        with col1:
+            st.markdown(
+                "<p style='font-size:14px; font-weight:normal; color:#444444; margin-top:0px;text-align:left;'>✔️ 작성에 필요한 요청사항 갯수를 설정해주세요.</p>",
+                unsafe_allow_html=True
+            )
+            
+        with col2:
+            st.markdown(
+                "<p style='font-size:14px; font-weight:normal; color:#444444; margin-top:0px;text-align:left;'>✔️ 보고서 저장을 위해 기준일자를 설정해주세요.</p>",
+                unsafe_allow_html=True
+            )
         col1, col2, col3 = st.columns([0.35, 0.35, 0.3])
         with col1:
             # 요청사항 갯수 입력 (1-9)
@@ -298,18 +310,7 @@ with st.expander("⚙️ 요청사항 설정 / 파일 업로드", expanded=st.se
                 st.session_state['check_setting_03']=False
                 st.session_state['html_report_03'] = ""
                 st.success(f"{st.session_state['num_requests_03']}개의 요청사항이 설정되었습니다.")
-        col1, col2 = st.columns([0.5, 0.5])
-        with col1:
-            st.markdown(
-                "<p style='font-size:14px; font-weight:normal; color:#444444; margin-top:35px;text-align:left;'>✔️ 작성에 필요한 요청사항 갯수를 설정해주세요.</p>",
-                unsafe_allow_html=True
-            )
-            
-        with col2:
-            st.markdown(
-                "<p style='font-size:14px; font-weight:normal; color:#444444; margin-top:35px;text-align:left;'>✔️ 보고서 저장을 위해 기준일자를 설정해주세요.</p>",
-                unsafe_allow_html=True
-            )
+
     with tab2:
 # 파일 업로드
         if github_info_loaded:
