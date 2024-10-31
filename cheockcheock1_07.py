ab17ec2ad0bfbb3bbd21402262f30ceb4bffbeab
soryhon <soryhon2@gmail.com>
2024-10-31 23:08:55 +0900

    Update cheockcheock1_07.py

--- cheockcheock1_07.py
+++ cheockcheock1_07.py
@@ -11,9 +11,9 @@ total_count, average_score, appraisal_data = bd.get_appraisal_data(file_path)
 if appraisal_data is not None:
     col1 , col2 = st.columns([0.5,0.5])
     with col1:
-      st.title(f"총 평가 건수: {total_count}건")
+      st.subtitle(f"총 평가 건수: {total_count}건")
     with col2:
-      st.write(f"평균 점수: {average_score:.2f}")
+      st.subtitle(f"평균 점수: {average_score:.2f}")
 
     # 시작일자와 종료일자를 설정
     start_date, end_date = bd.get_date_range(appraisal_data)
